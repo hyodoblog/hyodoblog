26d7d85269e166c6c300d5b2858481dd1b12baf6
yusei-hyodo <yusei.hyodo@youlib.co.jp>
2021-11-28 10:08:44 +0900

    :wrench: Tailwind cssの調整

--- blog/client/pages/index.vue
+++ blog/client/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     Index Page
+    <div class="text-center">aaaa</div>
   </div>
 </template>
--- blog/nuxt.config.ts
+++ blog/nuxt.config.ts
@@ -4,4 +4,15 @@ export default defineNuxtConfig({
   srcDir: 'client/',
 
   css: ['~/assets/css/tailwind.css'],
+
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        }
+      }
+    },
+  },
 })
